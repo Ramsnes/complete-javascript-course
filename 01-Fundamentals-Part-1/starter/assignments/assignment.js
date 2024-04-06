2fd6939f68c258fe148d086b66c494de36da0e92
Ramsnes <morten.ramfjord@gmail.com>
2024-04-06 23:02:45 +0200

    finish 01

---
+++ 01-Fundamentals-Part-1/starter/assignments/assignment.js
@@ -0,0 +1,37 @@
+URL = "https://codingheroes.io/assignments/values-and-variables.html"
+// Values and Variables
+let country = "Norway";
+let continent = "Europe";
+let population = "6";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// Data types 
+let isIsland = "false";
+let language;
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+
+// let, const and var
+language = "Norwegian";
+console.log(language);
+
+// Basic Operators
+let norwaySplit = population / 2;
+console.log(norwaySplit);
+
+norwaySplit ++;
+console.log(norwaySplit);
+
+const finland = 6;
+console.log(norwaySplit > finland);
+
+const averagePopulation = 33;
+console.log(norwaySplit < averagePopulation);
+
+console.log(country + " " + "is in Europe, and its" + " " + norwaySplit + " " + "million people" + " " + "speak Norwegian.");
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,7 @@
+let js = "amazing";
+// if (js === "amazing") alert("JS is fun!");
+// 40 + 8 + 23 - 10;
+console.log(40 + 8 + 23 - 10);
+console.log(40 + 8 + 23 - 10);
+
+let PI = 3.14;
