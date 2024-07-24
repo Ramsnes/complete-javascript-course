7c1ef2decb2b6c2c048909c1c470d6845d7c17e0
Ramsnes <115258511+Ramsnes@users.noreply.github.com>
2024-07-24 15:01:37 +0200

    cleaned 06 modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,36 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+//// OPENING MODAL ////
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+//// CLOSING MODAL ////
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// Pressing 'x' or/and overlay adds .hidden and closes modal
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+//// Handling an "Esc" Keypress Event ////
+// Closes modal/overlay by pressing 'Escape'(if .hidden class is NOT present)
+document.addEventListener('keydown', function (e) {
+  console.log(e); // key: 'Escape' - // Adding/logging (e) param lets ut look at events in console
+  console.log(e.key); // Escape
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    console.log('Esc was pressed and modal closed.');
+    closeModal();
+  }
+});
